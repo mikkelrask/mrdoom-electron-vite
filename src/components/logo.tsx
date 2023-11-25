816ce965e611e8f931040ec5037c245096b63282
Mikkel Rask <35899030+mikkelrask@users.noreply.github.com>
2023-11-25 09:55:31 +0100

    import rather than require

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 import React from "react";
+import logosrc from "../assets/logo.svg";
 //function to show logo from assets folder
 
-const logosrc = require("../assets/logo.svg");
 function Logo() {
   return (
     <>
